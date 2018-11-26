5f63b5bda9e27c9523d9533bba166b6cca78da73
Samuel Omanchi <omanchis@gmail.com>
2018-11-26 19:30:58 +0100

    major banks

---
+++ css/js/main.js
@@ -0,0 +1,8 @@
+$(".modal-wide").on("show.bs.modal", function () {
+    var height = $(window).height() - 200;
+    $(this).find(".modal-body").css("max-height", height);
+});
+$(".modal-wide").on("show.bs.modal", function () {
+    var height = $(window).height() - 200;
+    $(this).find(".modal-body").css("max-height", height);
+});
